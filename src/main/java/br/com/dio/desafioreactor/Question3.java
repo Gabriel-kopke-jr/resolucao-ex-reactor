15d52c70e33168541e33a523af40eb9a3f5c19a9
Gabriel K JR <gabrielkopke668@gmail.com>
2024-01-14 19:33:04 -0300

    feat: Cricao de metodos reativos

--- src/main/java/br/com/dio/desafioreactor/Question3.java
+++ src/main/java/br/com/dio/desafioreactor/Question3.java
@@ -8,6 +8,14 @@ public class Question3 {
     Verifica se o usuário passado é valido, caso seja retorna void, caso contrário deve disparar uma exception
     (para esse desafio vamos considerar que o usário é valido quando ele tem uma senha com mais de 8 caractéres)
      */
-    public Mono<Void> userIsValid(final User user){}
+    public Mono<Void> userIsValid(final User user){
+        Mono<User> mono = Mono.just(user);
+        Mono <?> result = mono.flatMap(user1 -> {
+            if (user1.password().length() > 8)
+                return Mono.empty();
+            else return Mono.error(new Exception());
+        });
+    return result.then();
+    }
 
 }
